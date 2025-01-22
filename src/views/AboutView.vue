3b20a623e2d8024102a861cbe928a5acce403f6c
german-rs <ing.germanriveros@gmail.com>
2025-01-21 21:17:49 -0300

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <h1 class="about__title">About</h1>
+    <section class="about__section">
+      <img
+        class="about__image"
+        src="../../public/german-rs.webp"
+        alt="Foto de perfil Germán Riveros"
+      />
+      <p class="about__description">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum libero molestiae esse illo!
+        Debitis similique quam nihil libero quos explicabo quasi et incidunt! Est sunt odio iure
+        autem quo tempora vero perspiciatis consequuntur, error placeat doloribus aliquid facilis
+        tenetur dolores assumenda sequi esse laborum vitae. Natus nesciunt repellat consectetur
+        eaque?
+      </p>
+    </section>
   </div>
 </template>
 
@@ -11,5 +25,23 @@
   justify-content: center;
   align-items: center;
   padding: 1em;
+
+  &__section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 1em;
+    gap: 2em;
+  }
+
+  &__image {
+    margin-top: 2em;
+    width: 300px;
+  }
+
+  &__description {
+    max-width: 600px;
+  }
 }
 </style>
